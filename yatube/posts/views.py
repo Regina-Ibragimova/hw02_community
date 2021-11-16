361326ec981eed0ac05bff4edf176231a28a5557
Regina-Ibragimova <ibragimovarn@yandex.ru>
2021-11-16 20:54:24 +0500

    добавила if в ссылку

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Group, Post
 def index(request):
     template = 'posts/index.html'
 
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
 
     title = 'Последние обновления'
     context = {
@@ -18,7 +18,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = group.group_filter.all()[:10]
     title = f'Записи сообщества {group}'
     context = {
         'group': group,
